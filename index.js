9403a3f1df2b5fdb99f0ef8bef4cedbf6e6dfc07
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2022-06-21 17:35:00 +0300

    commit tree 494bc48e1bb7bcf87dc913a50cd3b57a692aa948

--- index.js
+++ index.js
@@ -1,89 +1,83 @@
 // Your code here
 let createEmployeeRecord = function(row){
     return {
-    firstName: row[0],
-    familyName: row[1],
-    title: row[2],
-    payPerHour: row[3],
-    timeInEvents: [],
-    timeOutEvents: []
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
     }
-    }
-    
-    let createEmployees = function(employeeRowData) {
+}
+
+let createEmployeeRecords = function(employeeRowData) {
     return employeeRowData.map(function(row){
-    return createEmployeeRecord(row)
+        return createEmployeeRecord(row)
     })
-    }
-    
-    let createTimeInEvent = function(dateStamp){
-    let [date, hour] = dateStamp.split(' ');
-    
-    this.timeInEvents.push({
-    type: "TimeIn",
-    hour: parseInt(hour, 10),
-    date,
+}
+
+let createTimeInEvent = function(employee, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    employee.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
     })
-    
-    return this
-    }
-    
-    let createTimeOutEvent = function(dateStamp){
-    let [date, hour] = dateStamp.split(' ');
-    
-    this.timeOutEvents.push({
-    type: "TimeOut",
-    hour: parseInt(hour, 10),
-    date,
+
+    return employee
+}
+
+let createTimeOutEvent = function(employee, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    employee.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
     })
-    
-    return this
-    }
-    
-    let hoursWorkedOnDate = function(soughtDate){
-    let inEvent = this.timeInEvents.find(function(e){
-    return e.date === soughtDate
+
+    return employee
+}
+
+let hoursWorkedOnDate = function(employee, soughtDate){
+    let inEvent = employee.timeInEvents.find(function(e){
+        return e.date === soughtDate
     })
-    
-    let outEvent = this.timeOutEvents.find(function(e){
-    return e.date === soughtDate
+
+    let outEvent = employee.timeOutEvents.find(function(e){
+        return e.date === soughtDate
     })
-    
+
     return (outEvent.hour - inEvent.hour) / 100
-    }
-    
-    let wagesEarnedOnDate = function(dateSought){
-    let rawWage = hoursWorkedOnDate.call(this, dateSought)
-    * this.payPerHour
+}
+
+let wagesEarnedOnDate = function(employee, dateSought){
+    let rawWage = hoursWorkedOnDate(employee, dateSought)
+        * employee.payPerHour
     return parseFloat(rawWage.toString())
-    }
-    
-    let allWagesFor = function(){
-    let eligibleDates = this.timeInEvents.map(function(e){
-    return e.date
+}
+
+let allWagesFor = function(employee){
+    let eligibleDates = employee.timeInEvents.map(function(e){
+        return e.date
     })
-    
+
     let payable = eligibleDates.reduce(function(memo, d){
-    return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0)
-    
-    return payable
-    }
-    
-    let createEmployeeRecords = function(src) {
-    return src.map(function(row){
-    return createEmployeeRecord(row)
-    })
-    }
-    
-    let findEmployeebyFirstName = function(srcArray, firstName) {
-    return srcArray.find(function(rec){
-    return rec.firstName === firstName
-    })
-    }
-    
-    let calculatePayroll = function(arrayOfEmployeeRecords){
-    return arrayOfEmployeeRecords.reduce(function(memo, rec){
-    return memo + allWagesFor.call(rec)
+        return memo + wagesEarnedOnDate(employee, d)
     }, 0)
-    }
+
+    return payable
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor(rec)
+    }, 0)
+}
